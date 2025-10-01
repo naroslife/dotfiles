42e0e36451a8bb6acf849b919494671eb7c9a475
naroslife <robi54321@gmail.com>
2025-10-01 20:02:00 +0200

    Update flake description to indicate private version

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Modular Home Manager configuration with Nix flakes";
+  description = "Modular Home Manager configuration with Nix flakes - private version";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
